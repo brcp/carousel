abbb02fe3fe628489de8be632ff46918fc0a008f
brcp <59656356+brcp@users.noreply.github.com>
2023-03-09 17:48:54 -0600

    Update Carousel.tsx
    change  in carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,3 +1,4 @@
+import { Box } from '@mui/material';
 import Carousel from 'react-material-ui-carousel';
 
 interface CustomCarouselProps {
@@ -9,15 +10,27 @@ const CustomCarousel = ({ items }: CustomCarouselProps) => {
     <Carousel
       sx={{
         marginTop: '2em',
-        border: '2px solid #086',
+        // border: '2px solid #086',
         borderRadius: '4px',
+        // minHeight: '22em',
       }}
       animation="slide"
-      duration={2000}
-      // interval={4000}
+      interval={5000}
+      changeOnFirstRender={true}
+      stopAutoPlayOnHover={true}
+      navButtonsAlwaysVisible={true}
+      navButtonsProps={{
+        style: {
+          color: '#086',
+          backgroundColor: 'transparent',
+          opacity: 0.4,
+        },
+      }}
     >
-      {items.map(item => (
-        <>{item}</>
+      {items.map((item, index) => (
+        <Box key={index} sx={{ border: '3px solid transparent' }}>
+          {item}
+        </Box>
       ))}
     </Carousel>
   );
